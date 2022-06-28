99952598d4c36b5a60aa847b8c4c145910721af9
tu1014 <tu1014@naver.com>
2022-06-28 12:40:29 +0900

    create project

---
+++ greedy/greedy01.h
@@ -0,0 +1,17 @@
+//
+// Created by tu101 on 2022-06-28.
+//
+
+#ifndef ALGORITHM_GREEDY01_H
+#define ALGORITHM_GREEDY01_H
+
+
+#include <iostream>
+using namespace std;
+
+void printHello() {
+    cout << "Hello Greedy!" << endl;
+}
+
+
+#endif //ALGORITHM_GREEDY01_H

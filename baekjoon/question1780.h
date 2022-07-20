fb9b8754fda1d4bd6247b72e67ead1f7672729a3
안태욱 <tu1014@naver.com>
2022-07-20 09:10:28 +0900

    recursive

---
+++ baekjoon/question1780.h
@@ -0,0 +1,56 @@
+//
+// Created by 안태욱 on 2022/07/20.
+//
+
+#ifndef ALGORITHM_QUESTION1780_H
+#define ALGORITHM_QUESTION1780_H
+
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <stack>
+#include <vector>
+#include <list>
+#include <functional>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <tuple>
+using namespace std;
+
+void func(int x, int y, int size) {
+
+
+
+
+
+}
+
+
+
+int question() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    int arr[n][n];
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+
+    func(0, 0, n);
+
+
+    return 0;
+}
+
+
+#endif //ALGORITHM_QUESTION1780_H
